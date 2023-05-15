eed647e76bf8456ec2e5a8d7f5f870ca2222c07e
tlschdaud <tlschdaud1@naver.com>
2023-05-15 11:58:25 +0900

    설정

---
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -0,0 +1,24 @@
+package org.koreait.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path}")
+    private String fileuploadpath;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("main/index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + fileuploadpath);
+    }
+}
