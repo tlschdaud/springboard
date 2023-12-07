4d1a846953556087f47020a61dd0d69486abfc28
Shin ChongMyeong <tlschdaud1@naver.com>
2023-12-07 23:27:43 +0900

    게시판 작업 1207

--- src/main/java/org/koreait/configs/SecurityConfig.java
+++ src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.koreait.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -7,10 +8,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
     }
 
+    @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return w-> w.ignoring().requestMatchers("/css/**", "/js/**", "/images/**", "/errors/**");
     }
