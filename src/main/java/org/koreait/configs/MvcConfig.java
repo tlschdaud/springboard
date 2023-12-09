1bfde602257bad18004b1bb95a6e625085f3ce8d
Shin ChongMyeong <tlschdaud1@naver.com>
2023-12-09 09:08:33 +0900

    1209 게시판 작업

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -4,8 +4,12 @@ package org.koreait.configs;
 import lombok.RequiredArgsConstructor;
 import org.koreait.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -38,5 +42,18 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(siteConfigInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors");
+
+        return ms;
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() { // GET, POST외에 DELETE, PATCH, PUT
+        return new HiddenHttpMethodFilter();
     }
 }
--- src/main/java/org/koreait/controllers/members/JoinForm.java
+++ src/main/java/org/koreait/controllers/members/JoinForm.java
@@ -1,5 +1,7 @@
 package org.koreait.controllers.members;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +11,24 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class JoinForm {
-
+    @NotBlank
+    @Size(min = 6, max = 20)
     private String userId; // 아이디
+
+    @NotBlank
+    @Size(min = 8)
     private String userPw;
+
+    @NotBlank
     private String userPwRe;
+
+    @NotBlank
     private String userNm;
+
+    @NotBlank @Email
     private String email;
+
     private String mobile;
+
     private boolean[] agrees;
 }
