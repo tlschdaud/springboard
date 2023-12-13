abac45982879311a63f4142c815dfba23d42a9e3
Shin ChongMyeong <tlschdaud1@naver.com>
2023-12-13 23:24:49 +0900

    1213 작업

--- src/main/java/org/koreait/controllers/members/JoinValidator.java
+++ src/main/java/org/koreait/controllers/members/JoinValidator.java
@@ -55,7 +55,7 @@ public class JoinValidator implements Validator, MobileValidator, PasswordValida
             errors.rejectValue("userPwRe", "Validation.incorrect.userPwRe");
         }
 
-        // 4. 휴대전화번호(선택) - 입력된 경우 형식 체크
+        // 4. 휴대전화번호(선택) - 입력된 경우 형식 체크.
         // 5. 휴대전화번호가 입력된 경우 숫자만 추출해서 다시 커맨드 객체에 저장
         if(mobile != null && !mobile.isBlank()) {
             if(!mobileNumCheck(mobile)) {
